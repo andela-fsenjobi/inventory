22abf0deea619f8dbf9c664a40acd7a18f32e95b
Femi Senjobi <femi.senjobi@andela.com>
2016-07-19 09:54:08 +0100

    feat(products): add search form
    add serach form to the index page

--- spec/features/visit_homepage_spec.rb
+++ spec/features/visit_homepage_spec.rb
@@ -3,6 +3,8 @@ require "rails_helper"
 RSpec.feature "Visit Home Page" do
   scenario "expec to find rails" do
     visit "/"
-    expect(page).to have_content "Rails"
+    expect(page).to have_content "My Inventory"
+    expect(page).to have_css "#search-input"
+    expect(page).to have_css "#search-submit"
   end
 end

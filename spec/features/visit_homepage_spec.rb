fb86c991e25553d00eb331ad26a9790e9d2509c8
Femi Senjobi <femi.senjobi@andela.com>
2016-07-19 11:05:48 +0100

    update ui

--- spec/features/visit_homepage_spec.rb
+++ spec/features/visit_homepage_spec.rb
@@ -7,4 +7,13 @@ RSpec.feature "Visit Home Page" do
     expect(page).to have_css "#search-input"
     expect(page).to have_css "#search-submit"
   end
+
+  scenario "expec to find rails" do
+    visit root_path
+
+    fill_in "search-input", with: "femi"
+    click_button "#search-submit"
+
+    expect(page).to have_content "Femi"
+  end
 end
